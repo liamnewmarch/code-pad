2b5f24ade8ceb17a240449b9a1111cc943cc0eae
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2012-12-17 15:51:01 +0000

    Added support for tabbing with better tab styling (webkit/moz/o)

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,23 @@ $(function() {
 		}
 	});
 
-	$('.view textarea').keyup(function() {
-		var name = $(this).parent().attr('id').replace('view-', '')
+	$('.view textarea').keydown(function(e) {
+		if (e.keyCode == 9) {
+			var myValue = "\t";
+			var startPos = this.selectionStart;
+			var endPos = this.selectionEnd;
+			var scrollTop = this.scrollTop;
+			this.value = this.value.substring(0, startPos) + myValue + this.value.substring(endPos,this.value.length);
+			this.focus();
+			this.selectionStart = startPos + myValue.length;
+			this.selectionEnd = startPos + myValue.length;
+			this.scrollTop = scrollTop;
+			return false;
+		}
+	});
+
+	$('.view textarea').keyup(function(e) {
+		var name = $(this).parent().attr('id').replace('view-', '');
 		localStorage['fiddle-' + name] = $(this).val();
 	});
 
