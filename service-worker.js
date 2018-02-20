d78facc7271fc72bf651b6e1648e4eb4fadef7af
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2018-02-20 23:12:24 +0000

    Clean up CSS, remove normalize.

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,31 @@
 const cacheVersion = '1.0.0';
 
+const files = [
+  '.',
+  'index.html',
+  'assets/js/main.js',
+  'assets/css/styles.css',
+  'assets/css/_base.css',
+  'assets/css/_fiddle.css',
+  'assets/css/_menu.css',
+  'assets/css/_start.css',
+  'assets/css/_top-nav.css',
+  'assets/css/_view.css',
+  'bower_components/codemirror/lib/codemirror.css',
+  'bower_components/codemirror/lib/codemirror.js',
+  'bower_components/codemirror/theme/monokai.css',
+  'bower_components/codemirror/mode/css/css.js',
+  'bower_components/codemirror/mode/htmlmixed/htmlmixed.js',
+  'bower_components/codemirror/mode/javascript/javascript.js',
+  'bower_components/codemirror/mode/xml/xml.js',
+  'bower_components/angular/angular.min.js',
+  'bower_components/angular-sanitize/angular-sanitize.min.js',
+  'bower_components/angular-ui-codemirror/ui-codemirror.min.js',
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(cacheVersion).then(cache => {
-    return cache.addAll([
-      '.',
-      'index.html',
-      'assets/js/main.js',
-      'assets/css/styles.css',
-      'bower_components/codemirror/lib/codemirror.css',
-      'bower_components/codemirror/lib/codemirror.js',
-      'bower_components/codemirror/theme/monokai.css',
-      'bower_components/codemirror/mode/css/css.js',
-      'bower_components/codemirror/mode/htmlmixed/htmlmixed.js',
-      'bower_components/codemirror/mode/javascript/javascript.js',
-      'bower_components/codemirror/mode/xml/xml.js',
-      'bower_components/angular/angular.min.js',
-      'bower_components/angular-sanitize/angular-sanitize.min.js',
-      'bower_components/angular-ui-codemirror/ui-codemirror.min.js',
-    ]);
+    return cache.addAll(files);
   }));
 });
 
