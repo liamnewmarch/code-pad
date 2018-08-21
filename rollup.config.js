ae5fcb6ebed0a27a1ec4593672be88d8e4d63974
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2018-08-21 10:50:15 +0100

    Add deploy task, minify JS during production build.

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,30 @@ import commonjs from 'rollup-plugin-commonjs';
 import css from 'rollup-plugin-css-only'
 import replace from 'rollup-plugin-replace';
 import resolve from 'rollup-plugin-node-resolve';
-import { uglify } from 'rollup-plugin-uglify';
+import uglify from 'rollup-plugin-uglify';
 import vue from 'rollup-plugin-vue';
+
+const plugins = [
+  commonjs(),
+  css(),
+  vue({
+    css: false,
+  }),
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    'process.env.VUE_ENV': '"browser"',
+  }),
+  resolve({
+    extensions: ['.js', '.vue'],
+  }),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(uglify());
+}
 
 export default {
   input: 'app/index.js',
@@ -13,22 +35,5 @@ export default {
     format: 'iife',
     sourcemap: false,
   },
-  plugins: [
-    commonjs(),
-    css(),
-    vue({
-      css: false,
-    }),
-    babel({
-      exclude: 'node_modules/**',
-    }),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-      'process.env.VUE_ENV': JSON.stringify(process.env.VUE_ENV),
-    }),
-    resolve({
-      extensions: ['.js', '.vue'],
-    }),
-    // uglify(),
-  ],
+  plugins,
 };
