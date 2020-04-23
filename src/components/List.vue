51f4745a899edaad87b408339fef0aeaa6864d9b
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2020-04-23 22:40:46 +0100

    Improve list view grid, add filtering

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,8 +1,13 @@
 <script>
 export default {
+  data() {
+    return {
+      filterText: '',
+    };
+  },
   computed: {
     projects() {
-      return this.sort('-created', Object.values(this.$store.state.projects));
+      return Object.values(this.$store.state.projects);
     },
   },
   methods: {
@@ -10,11 +15,17 @@ export default {
       const key = await this.$store.dispatch('addProject');
       this.$router.push({ name: 'editor', params: { key, type: 'html' }});
     },
+    filter(key, text, array) {
+      text = text.trim().toLowerCase();
+      return array.filter((item) => {
+        return text ? item[key].trim().toLowerCase().includes(text) : true;
+      });
+    },
     sort(key, array) {
       const ascending = key.startsWith('-');
       if (ascending) key = key.substr(1);
       const fn = ({ [key]: a }, { [key]: b }) => a < b ? -1 : a > b ? 1 : 0;
-      return ascending ? array.sort(fn).reverse() : array.sort(fn);
+      return ascending ? [...array].sort(fn).reverse() : [...array].sort(fn);
     },
   },
 };
@@ -22,6 +33,13 @@ export default {
 
 <template>
   <section class="view list">
+    <input
+      v-if="projects.length > 12"
+      v-model="filterText"
+      class="list__filter"
+      placeholder="Filter projects"
+      type="text"
+    >
     <div class="list__items">
       <button
         class="list__item list__item--add"
@@ -30,7 +48,7 @@ export default {
         New project
       </button>
       <RouterLink
-        v-for="(project, index) of projects"
+        v-for="(project, index) of sort('-created', filter('name', filterText, projects))"
         :key="index"
         class="list__item button"
         tag="button"
@@ -52,10 +70,20 @@ export default {
 </template>
 
 <style>
+.list__filter {
+  border: 1px solid #fff4;
+  border-radius: .2rem;
+  margin: 1rem;
+  padding: 1rem 2rem;
+}
+
+.list__filter:focus {
+  border: 1px solid #fff;
+}
+
 .list__items {
-  display: flex;
-  flex-flow: row wrap;
-  flex-shrink: 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
   padding: 1rem 0 0 1rem;
 }
 
@@ -63,9 +91,11 @@ export default {
   background: #444;
   border-radius: .2rem;
   flex: 1 0 8rem;
+  hyphens: auto;
   margin: 0 1rem 1rem 0;
   padding: 3rem 1rem;
   text-align: center;
+  word-break: break-all;
 }
 
 .list__item--add {
