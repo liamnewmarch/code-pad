132488a97ae0dee72c5ab404a807c1a72af3e568
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2020-12-16 21:48:59 +0000

    Fix double-quoted string

--- src/components/Result.vue
+++ src/components/Result.vue
@@ -38,7 +38,7 @@ export default {
         for (const key of this.$data.consoleMethods) {
           targetWindow.console[key] = (...args) => this.logging.push(...args);
         }
-        const url = JSON.stringify('data:text/javascript;base64,' + btoa(javascript));
+        const url = 'data:text/javascript;base64,' + btoa(javascript);
         await targetWindow.eval(`import('${url}');`);
       } catch (error) {
         this.logging.push('message' in error ? error.message : error);
