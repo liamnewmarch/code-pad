0b2c347a566e6e16a2a3b5c37b3b32a33bbd6eb1
Liam Newmarch <liamnewmarch@users.noreply.github.com>
2026-08-26 15:40:06 +0100

    Whitelist auth redirect

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       workbox: {
         globPatterns: ["**/*.{js,css,html,png}"],
+        navigateFallbackDenylist: [/^\/__\//],
       },
       manifest: false,
     }),
